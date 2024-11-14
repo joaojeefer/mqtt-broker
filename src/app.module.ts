7effdc256736f34af55b2304b7ac506585c391bd
João Jeefer da Silva Félix <joao.jeefer@gmail.com>
2024-11-13 23:03:15 -0300

    feat: :sparkles: add configure-module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { MqttModule } from './mqtt/mqtt.module';
 import { DbModule } from './db/db.module';
 
 @Module({
-  imports: [DbModule, MqttModule],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), DbModule, MqttModule],
 })
 export class AppModule {}

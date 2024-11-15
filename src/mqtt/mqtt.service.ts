8ca6174540c152f95f7c05a696de05de9eb194ee
dfrancio <diego@falker.com.br>
2024-11-15 13:41:30 -0300

    first implementation of unpack data

--- src/mqtt/mqtt.service.ts
+++ src/mqtt/mqtt.service.ts
@@ -1,17 +1,19 @@
 import { Injectable } from '@nestjs/common';
 import { connect, MqttClient } from 'mqtt';
 
+import { BinaryUnpacker } from './binary-unpacker';
+
 @Injectable()
 export class MqttService {
   public readonly mqttClient: MqttClient;
-
-  constructor() {
-    this.mqttClient = connect(process.env.connectUrl, {
-      clientId: process.env.clientId || null,
+  
+  constructor(private readonly unpacker: BinaryUnpacker) {
+      this.mqttClient = connect(process.env.connectUrl, {
+      //clientId: process.env.clientId || null,
       clean: true,
       connectTimeout: parseInt(process.env.connectTimeout, 10),
-      username: process.env.username,
-      password: process.env.password,
+      //username: process.env.username,
+      //password: process.env.password,
       reconnectPeriod: parseInt(process.env.reconnectPeriod, 10),
     });
 
@@ -19,16 +21,27 @@ export class MqttService {
       console.log('Connected to MQTT server');
     });
 
-    this.mqttClient.on('error', () => {
+    this.mqttClient.on('error', (error) => {
       console.error('Error in connecting to CloudMQTT');
+      console.error(error);
     });
 
-    this.mqttClient.subscribe('/from-device', { qos: 1 });
+    //this.mqttClient.subscribe('/from-device', { qos: 1 });
+    this.mqttClient.subscribe('IoTSensors/Machine/#', { qos: 1 });    
 
     this.mqttClient.on('message', (topic, message) => {
       // Descompatar a mensagem recebida.
-      console.log(`New message received from topic: ${topic}`);
-      console.log(message.toString());
+      console.log(`New message received from topic: ${topic}\n`);
+      //console.log(message.toString());
+      
+      console.log("--message size--");
+      console.log(message.length);
+
+    
+      const buffer = Buffer.from(message); // Converte a mensagem para buffer
+      const unpackedData = this.unpacker.unpack(buffer); // Faz o unpack
+      console.log('Mensagem descompactada:', unpackedData);
+          
     });
   }
 }
